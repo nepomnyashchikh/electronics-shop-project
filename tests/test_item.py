1ce741107de81efabf8750f7574c2d535fb156cb
nepomnyashchikh <nepomnyashchikh.a@mail.ru>
2023-06-04 15:39:02 +0700

    написал тесты для новых методов в `tests/test_item.py`

--- tests/test_item.py
+++ tests/test_item.py
@@ -21,6 +21,35 @@ def test_apply_discount(class_item):
    item.apply_discount()
    assert item.price == 20000
 
+def test_name():
+   # Создаем класс для проверки новых методов
+   item = Item('Телефон', 10000, 5)
+
+   # Exception: Длина наименования товара превышает 10 символов.
+
+   assert item.name == 'Телефон'
+
+   # проверяем сеттер товар, у которого наименование менее 10 символов
+   item.name = 'Смартфон'
+   assert item.name == 'Смартфон'
+   with pytest.raises(Exception):
+     item.name = 'СуперСмартфон'
+
+def test_instantiate_from_csv():
+   # Запускаем метод вызывающий класс из файла
+   Item.instantiate_from_csv()
+   # Проверяем корректность работы метода, подсчетом записей
+   assert len(Item.all) == 5  # в файле 5 записей с данными по товарам
+
+   item6 = Item.all[0]
+   assert item6.name == 'Шпинат'
+
+def test_string_to_number():
+   # проверка стаческого метода, который возвращает число из числа-строки
+   assert Item.string_to_number('5') == 5
+   assert Item.string_to_number('5.0') == 5
+   assert Item.string_to_number('5.5') == 5
+
 
 
 
